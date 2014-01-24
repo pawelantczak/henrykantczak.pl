5cff963ca2d94615073abe692381cbfac867c44d
Paweł Antczak <pawel@antczak.org>
2014-01-24 09:15:01 +0100

    Init

---
+++ menu_dol.js
@@ -0,0 +1,7 @@
+﻿document.write('<a href="index.html"><font color="#FFFFFF">strona główna</font></a>&nbsp;&nbsp;&nbsp;&nbsp;'); 
+document.write('<a href="osobie.html"><font color="#FFFFFF">o sobie</font></a>&nbsp;&nbsp;&nbsp;&nbsp;'); 
+document.write('<a href="program_wyborczy.html"><font color="#FFFFFF">program wyborczy</font></a>&nbsp;&nbsp;&nbsp;&nbsp;');
+document.write('<a href="referencje.html"><font color="#FFFFFF">referencje</font></a>&nbsp;&nbsp;&nbsp;&nbsp;');
+document.write('<a href="galeria.html"><font color="#FFFFFF">galeria</font></a>&nbsp;&nbsp;&nbsp;&nbsp;');
+document.write('<a href="linki.html"><font color="#FFFFFF">linki</font></a>&nbsp;&nbsp;&nbsp;&nbsp;');
+document.write('<a href="kontakt.html"><font color="#FFFFFF">kontakt</font></a>'); 
---
+++ menu_gora.js
@@ -0,0 +1,7 @@
+﻿document.write(' <li><a class="item active" href="index.html">strona główna</a></li>');
+document.write(' <li><a class="item" href="osobie.html">o sobie</a></li>'); 
+document.write('  <li><a class="item" href="program_wyborczy.html">program wyborczy</a></li>');
+document.write('<li><a class="item" href="referencje.html">referencje</a></li>');
+document.write(' <li><a class="item" href="galeria.html">galeria</a></li>');
+document.write('<li><a class="item" href="kontakt.html">kontakt</a></li>'); 
+
